bbadbdbd0b28ec47b76ff10f7c5d16465e9b46fa
m-waqas-taj <mohammad.waqas@devsinc.com>
2021-08-26 17:53:44 +0500

    reading files with CSV

--- read_file.rb
+++ read_file.rb
@@ -1,10 +1,5 @@
 require_relative 'weather'
-
-DATE_INDEX = 0
-MAX_TEMP_INDEX = 1
-MIN_TEMP_INDEX = 3
-MAX_HUMID_INDEX = 7
-AVG_HUMID_INDEX = 8
+require 'csv'
 
 class Read
 
@@ -16,14 +11,14 @@ class Read
     begin
       Dir.chdir(path) do
         Dir.glob(files).each do |file|
-          File.readlines(file).each do |line|
-            line = line.split('\n').join.split(',')
-            if line[DATE_INDEX] != "PKT" && line[MAX_TEMP_INDEX] != ""
-              date = line[DATE_INDEX]
-              max_temp = line[MAX_TEMP_INDEX].to_i
-              min_temp = line[MIN_TEMP_INDEX].to_i
-              max_humid = line[MAX_HUMID_INDEX].to_i
-              avg_humid = line[AVG_HUMID_INDEX].to_i
+          result = CSV.parse(File.read(file), headers: true, converters: :numeric)
+          result.each do |row|
+            if !row["Max TemperatureC"].nil?
+              date = row[0]
+              max_temp = row["Max TemperatureC"]
+              min_temp = row["Min TemperatureC"]
+              max_humid = row["Max Humidity"]
+              avg_humid = row[" Mean Humidity"]
               @data << Weather.new(date, max_temp, min_temp, max_humid, avg_humid)
             end
           end
